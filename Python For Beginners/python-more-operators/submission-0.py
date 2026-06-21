2a0dd3b69b33e37b4706099056b03163c0e614b3
remzysanchez <156636545+remzysanchez@users.noreply.github.com>
2026-06-21 03:50:01 -0700

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,13 @@
+a, b, c = 2, 8, 5
+
+challenge_one = a * b // c
+print(challenge_one)
+
+challenge_two = a * b % c
+print(challenge_two)
+
+challenge_three = a ** b
+print(challenge_three)
+
+challenge_four = b ** c
+print(challenge_four)
